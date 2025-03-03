d898dd82f5ae355cdcc9d3bed51bbe518b129238
dieghernan <dieghernan@users.noreply.github.com>
2025-03-03 00:32:18 +0000

    Deploying to gh-pages from @ dieghernan/chulapa-101@6375486ff8b34fad8d2c8e35769cec86b64f4650 🚀

--- assets/js/fusejs/fusejs-store.js
+++ assets/js/fusejs/fusejs-store.js
@@ -16,7 +16,7 @@ var store = [{
       },{
         "title": "First post!",
         "subtitle": null,
-        "excerpt": "This is my first post, how exciting!\n",
+        "excerpt": "This is my first post, how exciting!",
         "content": "This is my first post, how exciting!  ",
         "categories": [],
         "date": "2015-01-04",
@@ -26,7 +26,7 @@ var store = [{
       },{
         "title": "Pirates arrrr",
         "subtitle": null,
-        "excerpt": "Piracy is typically an act of robbery or criminal violence at sea. The term can include acts committed on land, in the air, or in other major bodies of water or on a shore. It does not normally include crimes committed against persons traveling on the same vessel as the perpetrator (e.g. one passenger stealing from others on the same vessel). The term has been used throughout history to refer to raids across land borders by non-state agents.\n",
+        "excerpt": "Piracy is typically an act of robbery or criminal violence at sea. The term can include acts committed on land, in the air, or in other major bodies of water or on a shore. It does not normally include crimes committed against persons traveling on the same vessel as the perpetrator (e.g. one passenger stealing from others on the same vessel). The term has been used throughout history to refer to raids across land borders by non-state agents.",
         "content": "Piracy is typically an act of robbery or criminal violence at sea. The term can include acts committed on land, in the air, or in other major bodies of water...",
         "categories": [],
         "date": "2015-01-15",
@@ -36,7 +36,7 @@ var store = [{
       },{
         "title": "Soccer",
         "subtitle": "Best sport ever!",
-        "excerpt": "From Wikipedia:\n",
+        "excerpt": "From Wikipedia:",
         "content": "From Wikipedia: Association football, more commonly known as football or soccer,[2] is a sport played between two teams of eleven players with a spherical ball. It is played by 250...",
         "categories": [],
         "date": "2015-01-19",
@@ -46,7 +46,7 @@ var store = [{
       },{
         "title": "Dear diary",
         "subtitle": null,
-        "excerpt": "What is it with that Mary girl?  Dragging me to school every day. As if I had a choice.  What you don’t hear in those nursery rhymes is that she starves me if I don’t go to school with her; it’s the only way I can stay alive!  I’m thinking about being adopted by Little Bo Peep, sure I may get lost, but anything is better than being with Mary and those little brats at school (shudder, shudder).\n",
+        "excerpt": "What is it with that Mary girl?  Dragging me to school every day. As if I had a choice.  What you don’t hear in those nursery rhymes is that she starves me if I don’t go to school with her; it’s the only way I can stay alive!  I’m thinking about being adopted by Little Bo Peep, sure I may get lost, but anything is better than being with Mary and those little brats at school (shudder, shudder).",
         "content": "What is it with that Mary girl? Dragging me to school every day. As if I had a choice. What you don’t hear in those nursery rhymes is that she...",
         "categories": [],
         "date": "2015-01-27",
@@ -56,7 +56,7 @@ var store = [{
       },{
         "title": "To be",
         "subtitle": "... or not to be?",
-        "excerpt": "To be, or not to be–that is the question:\nWhether ‘tis nobler in the mind to suffer\nThe slings and arrows of outrageous fortune\nOr to take arms against a sea of troubles\nAnd by opposing end them. To die, to sleep–\nNo more–and by a sleep to say we end\nThe heartache, and the thousand natural shocks\nThat flesh is heir to. ‘Tis a consummation\nDevoutly to be wished. To die, to sleep–\nTo sleep–perchance to dream: ay, there’s the rub,\nFor in that sleep of death what dreams may come\nWhen we have shuffled off this mortal coil,\nMust give us pause. There’s the respect\nThat makes calamity of so long life.\nFor who would bear the whips and scorns of time,\nTh’ oppressor’s wrong, the proud man’s contumely\nThe pangs of despised love, the law’s delay,\nThe insolence of office, and the spurns\nThat patient merit of th’ unworthy takes,\nWhen he himself might his quietus make\nWith a bare bodkin? Who would fardels bear,\nTo grunt and sweat under a weary life,\nBut that the dread of something after death,\nThe undiscovered country, from whose bourn\nNo traveller returns, puzzles the will,\nAnd makes us rather bear those ills we have\nThan fly to others that we know not of?\nThus conscience does make cowards of us all,\nAnd thus the native hue of resolution\nIs sicklied o’er with the pale cast of thought,\nAnd enterprise of great pitch and moment\nWith this regard their currents turn awry\nAnd lose the name of action. – Soft you now,\nThe fair Ophelia! – Nymph, in thy orisons\nBe all my sins remembered.\n",
+        "excerpt": "To be, or not to be–that is the question:Whether ‘tis nobler in the mind to sufferThe slings and arrows of outrageous fortuneOr to take arms against a sea of troublesAnd by opposing end them. To die, to sleep–No more–and by a sleep to say we endThe heartache, and the thousand natural shocksThat flesh is heir to. ‘Tis a consummationDevoutly to be wished. To die, to sleep–To sleep–perchance to dream: ay, there’s the rub,For in that sleep of death what dreams may comeWhen we have shuffled off this mortal coil,Must give us pause. There’s the respectThat makes calamity of so long life.For who would bear the whips and scorns of time,Th’ oppressor’s wrong, the proud man’s contumelyThe pangs of despised love, the law’s delay,The insolence of office, and the spurnsThat patient merit of th’ unworthy takes,When he himself might his quietus makeWith a bare bodkin? Who would fardels bear,To grunt and sweat under a weary life,But that the dread of something after death,The undiscovered country, from whose bournNo traveller returns, puzzles the will,And makes us rather bear those ills we haveThan fly to others that we know not of?Thus conscience does make cowards of us all,And thus the native hue of resolutionIs sicklied o’er with the pale cast of thought,And enterprise of great pitch and momentWith this regard their currents turn awryAnd lose the name of action. – Soft you now,The fair Ophelia! – Nymph, in thy orisonsBe all my sins remembered.",
         "content": "To be, or not to be–that is the question: Whether ‘tis nobler in the mind to suffer The slings and arrows of outrageous fortune Or to take arms against a...",
         "categories": [],
         "date": "2015-02-13",
@@ -66,7 +66,7 @@ var store = [{
       },{
         "title": "Test markdown",
         "subtitle": "Each post also has a subtitle",
-        "excerpt": "You can write regular markdown here and Jekyll will automatically convert it to a nice webpage.  I strongly encourage you to take 5 minutes to learn how to write in markdown - it’ll teach you how to transform regular text into bold/italics/headings/tables/etc.\n",
+        "excerpt": "You can write regular markdown here and Jekyll will automatically convert it to a nice webpage.  I strongly encourage you to take 5 minutes to learn how to write in markdown - it’ll teach you how to transform regular text into bold/italics/headings/tables/etc.",
         "content": "You can write regular markdown here and Jekyll will automatically convert it to a nice webpage. I strongly encourage you to take 5 minutes to learn how to write in...",
         "categories": [],
         "date": "2015-02-20",
@@ -76,7 +76,7 @@ var store = [{
       },{
         "title": "Flake it till you make it",
         "subtitle": "Excerpt from Soulshaping by Jeff Brown",
-        "excerpt": "Under what circumstances should we step off a path? When is it essential that we finish what we start? If I bought a bag of peanuts and had an allergic reaction, no one would fault me if I threw it out. If I ended a relationship with a woman who hit me, no one would say that I had a commitment problem. But if I walk away from a seemingly secure route because my soul has other ideas, I am a flake?\n",
+        "excerpt": "Under what circumstances should we step off a path? When is it essential that we finish what we start? If I bought a bag of peanuts and had an allergic reaction, no one would fault me if I threw it out. If I ended a relationship with a woman who hit me, no one would say that I had a commitment problem. But if I walk away from a seemingly secure route because my soul has other ideas, I am a flake?",
         "content": "Under what circumstances should we step off a path? When is it essential that we finish what we start? If I bought a bag of peanuts and had an allergic...",
         "categories": [],
         "date": "2015-02-26",
@@ -86,7 +86,7 @@ var store = [{
       },{
         "title": "Urban art",
         "subtitle": "Creativity is everywhere",
-        "excerpt": "Urban artists can use anything as a canvas to express their art\n",
+        "excerpt": "Urban artists can use anything as a canvas to express their art",
         "content": "Urban artists can use anything as a canvas to express their art      Picture by Zarateman  ",
         "categories": ["picture"],
         "date": "2018-11-09",
@@ -96,7 +96,7 @@ var store = [{
       },{
         "title": "Dean Village",
         "subtitle": null,
-        "excerpt": "\n  Dean Village (from dene, meaning ‘deep valley’) is a former village immediately northwest of the city centre of Edinburgh, Scotland. It was known as the “Water of Leith Village” and was the centre of a successful grain milling area for more than 800 years. At one time there were no fewer than eleven working mills there, driven by the strong currents of the Water of Leith.\n\n  From Wikipedia\n\n",
+        "excerpt": "  Dean Village (from dene, meaning ‘deep valley’) is a former village immediately northwest of the city centre of Edinburgh, Scotland. It was known as the “Water of Leith Village” and was the centre of a successful grain milling area for more than 800 years. At one time there were no fewer than eleven working mills there, driven by the strong currents of the Water of Leith.  From Wikipedia",
         "content": "Dean Village (from dene, meaning ‘deep valley’) is a former village immediately northwest of the city centre of Edinburgh, Scotland. It was known as the “Water of Leith Village” and...",
         "categories": ["picture"],
         "date": "2019-05-02",
@@ -106,7 +106,7 @@ var store = [{
       },{
         "title": "A landing page",
         "subtitle": "With image header",
-        "excerpt": "A simple landing page with an image on top. Transparencies (png) works better on this combination.\n",
+        "excerpt": "A simple landing page with an image on top. Transparencies (png) works better on this combination.",
         "content": "A simple landing page with an image on top. Transparencies (png) works better on this combination.  ",
         "categories": ["landing-page"],
         "date": "2019-08-03",
@@ -116,7 +116,7 @@ var store = [{
       },{
         "title": "The Acueduct of Segovia",
         "subtitle": "20 centuries ago",
-        "excerpt": "The Aqueduct of Segovia (Spanish: Acueducto de Segovia; more accurately, the aqueduct bridge) is a Roman aqueduct in Segovia, Spain. It is one of the best-preserved elevated Roman aqueducts and the foremost symbol of Segovia, as evidenced by its presence on the city’s coat of arms.\n",
+        "excerpt": "The Aqueduct of Segovia (Spanish: Acueducto de Segovia; more accurately, the aqueduct bridge) is a Roman aqueduct in Segovia, Spain. It is one of the best-preserved elevated Roman aqueducts and the foremost symbol of Segovia, as evidenced by its presence on the city’s coat of arms.",
         "content": "The Aqueduct of Segovia (Spanish: Acueducto de Segovia; more accurately, the aqueduct bridge) is a Roman aqueduct in Segovia, Spain. It is one of the best-preserved elevated Roman aqueducts and...",
         "categories": ["picture"],
         "date": "2019-11-27",
@@ -126,7 +126,7 @@ var store = [{
       },{
         "title": "Current skin",
         "subtitle": "Showing al the different Bootstrap components available",
-        "excerpt": "This is a demo page showing the different components of Bootstrap and how they look on this site under the current configuration.\n",
+        "excerpt": "This is a demo page showing the different components of Bootstrap and how they look on this site under the current configuration.",
         "content": "This is a demo page showing the different components of Bootstrap and how they look on this site under the current configuration. Previous The Acueduct of Segovia The Acueduct o......",
         "categories": ["skins"],
         "date": "2020-12-31",
